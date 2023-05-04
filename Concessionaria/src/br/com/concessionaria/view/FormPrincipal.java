92f3638aa5a1419a52f453fad4fb766d8c3741d8
Lucas Dantas <luscasedudantas@gmail.com>
2023-05-03 22:13:04 -0300

    Definicao de layout no form principal

--- Concessionaria/src/br/com/concessionaria/view/FormPrincipal.java
+++ Concessionaria/src/br/com/concessionaria/view/FormPrincipal.java
@@ -17,13 +17,21 @@ import javax.swing.SwingConstants;
 import java.awt.Component;
 import javax.swing.JTabbedPane;
 import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.Label;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.CardLayout;
 
 public class FormPrincipal extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-	static JMenu mnCadastro;
-	static JMenu mnRelatorio;
-	static JMenu mnBackup;
 	private JPanel contentPane;
 
 	/**
@@ -47,51 +55,89 @@ public class FormPrincipal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FormPrincipal() {
+	public FormPrincipal() {    
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1040, 564);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		//Instancia o contentPane
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		//Instancia o painel
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 1004, 503);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
+		/*
+		 * Menu bar itens
+		 */
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 1004, 36);
-		panel.add(menuBar);
+		setJMenuBar(menuBar);
 		
-		mnCadastro = new JMenu("Cadastro");
+		JMenu mnCadastro = new JMenu("Cadastro");
 		mnCadastro.setPreferredSize(new Dimension(90, 26));
 		menuBar.add(mnCadastro);
 		
+		JMenu mnRelatorio = new JMenu("Relatório");
+		mnRelatorio.setPreferredSize(new Dimension(90, 26));
+		menuBar.add(mnRelatorio);
+		
+		JMenu mnBackup = new JMenu("Backup");
+		mnBackup.setPreferredSize(new Dimension(90, 26));
+		menuBar.add(mnBackup);
+		
+		/*
+		 * Instancia o contentPane
+		 */
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		/*
+		 * Instancia o painel Geral card layout
+		 */
+		JPanel geralPanel = new JPanel();
+		geralPanel.setBounds(10, 11, 1004, 486);
+		contentPane.add(geralPanel);
+		geralPanel.setLayout(new CardLayout(0, 0));
+		CardLayout layout = (CardLayout)(geralPanel.getLayout());
+		
+		/*
+		 * Painel de veículos
+		 */
+		Veiculos veiculosPanel = new Veiculos();
+		veiculosPanel.setBounds(10, 11, 1004, 485);
+		geralPanel.add(veiculosPanel, "veiculos");
+		veiculosPanel.setLayout(null);
+		veiculosPanel.setVisible(false);
+		
+		/*
+		 * Painel de clientes
+		 */
+		JPanel clientesPanel = new JPanel();
+		geralPanel.add(clientesPanel, "clientes");
+		clientesPanel.setLayout(null);
+		
+		/*
+		 * Menu Cadastro itens
+		 */
 		JMenuItem mnItemClientes = new JMenuItem("Clientes");
+		mnItemClientes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layout.show(geralPanel, "clientes");
+			}
+		});
 		mnCadastro.add(mnItemClientes);
 		
-		JMenuItem mnItemFuncionarios = new JMenuItem("Funcionários");
-		mnCadastro.add(mnItemFuncionarios);
+		JMenuItem mnItemColaboradores = new JMenuItem("Colaboradores");
+		mnCadastro.add(mnItemColaboradores);
 		
 		JMenuItem mnItemServicos = new JMenuItem("Serviços");
 		mnCadastro.add(mnItemServicos);
 		
 		JMenuItem mnItemVeiculos = new JMenuItem("Veículos");
-		mnItemVeiculos.setHorizontalTextPosition(SwingConstants.LEFT);
-		mnItemVeiculos.setHorizontalAlignment(SwingConstants.LEFT);
+		mnItemVeiculos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				layout.show(geralPanel, "veiculos");
+			}
+		});
 		mnCadastro.add(mnItemVeiculos);
 		
-		mnRelatorio = new JMenu("Relatório");
-		mnRelatorio.setPreferredSize(new Dimension(90, 26));
-		menuBar.add(mnRelatorio);
-		
-		mnBackup = new JMenu("Backup");
-		mnBackup.setPreferredSize(new Dimension(90, 26));
-		menuBar.add(mnBackup);
+		JLabel lblNewLabel = new JLabel("testando layout");
+		lblNewLabel.setBounds(358, 31, 131, 93);
+		clientesPanel.add(lblNewLabel);
 		
 	}
 }
